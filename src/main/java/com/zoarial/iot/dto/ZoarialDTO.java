0afc06902220d45ad9b73a9b56fd122d43811971
Hunter White <HWB94@protonmail.com>
2022-08-21 15:09:58 -0400

    Started implementing the NetworkArbiter.
    Currently, the server does not work and is untested.

--- src/main/java/com/zoarial/iot/dto/ZoarialDTO.java
+++ src/main/java/com/zoarial/iot/dto/ZoarialDTO.java
@@ -1,78 +1,95 @@
 package com.zoarial.iot.dto;
 
-import lombok.Value;
-import me.zoarial.NetworkArbiter.annotations.ZoarialNetworkObject;
+import lombok.Data;
+import me.zoarial.networkArbiter.annotations.ZoarialNetworkObject;
+import me.zoarial.networkArbiter.annotations.ZoarialObjectElement;
 
-import java.util.ArrayList;
-import java.util.Optional;
 import java.util.UUID;
 
 public enum ZoarialDTO {;
 
     public enum V1 {;
+        public enum Request {;
 
-        // Request to complete action
-        @ZoarialNetworkObject
-        public static class Action {
-            final static String[] order = {"uuid", "otp", "args"};
-            UUID uuid;
-            Optional<Integer> otp;
-            Optional<ArrayList<String>> args;
-        }
+            // Request to complete action
+            @Data
+            @ZoarialNetworkObject
+            public static class Action {
+                @ZoarialObjectElement(placement = 1)
+                UUID uuid;
+                @ZoarialObjectElement(placement = 2, optional = true)
+                Short otp;
+                @ZoarialObjectElement(placement = 3)
+                String data;
+            }
 
-        // Request to update info about an action
-        @ZoarialNetworkObject
-        public static class UpdateActionSecurityLevel {
-            final static String[] order = {"uuid", "property", "level"};
-            UUID uuid;
-            String property;
-            byte level;
-        }
+            // Request to update info about an action
+            @Data
+            @ZoarialNetworkObject
+            public static class UpdateActionSecurityLevel {
+                @ZoarialObjectElement(placement = 1)
+                UUID uuid;
+                @ZoarialObjectElement(placement = 2)
+                String property;
+                @ZoarialObjectElement(placement = 3)
+                byte level;
+            }
 
-        @ZoarialNetworkObject
-        public static class UpdateActionEncrypt {
-            final static String[] order = {"uuid", "property", "encrypt"};
-            UUID uuid;
-            String property;
-            boolean encrypt;
-        }
+            @Data
+            @ZoarialNetworkObject
+            public static class UpdateActionEncrypt {
+                @ZoarialObjectElement(placement = 1)
+                UUID uuid;
+                @ZoarialObjectElement(placement = 2)
+                String property;
+                @ZoarialObjectElement(placement = 3)
+                boolean encrypt;
+            }
 
-        @ZoarialNetworkObject
-        public static class UpdateActionLocal {
-            final static String[] order = {"uuid", "property", "local"};
-            UUID uuid;
-            String property;
-            boolean local;
-        }
+            @Data
+            @ZoarialNetworkObject
+            public static class UpdateActionLocal {
+                @ZoarialObjectElement(placement = 1)
+                UUID uuid;
+                @ZoarialObjectElement(placement = 2)
+                String property;
+                @ZoarialObjectElement(placement = 3)
+                boolean local;
+            }
 
-        @ZoarialNetworkObject
-        public static class UpdateActionDescription {
-            final static String[] order = {"uuid", "property", "description"};
-            UUID uuid;
-            String property;
-            String description;
-        }
+            @Data
+            @ZoarialNetworkObject
+            public static class UpdateActionDescription {
+                @ZoarialObjectElement(placement = 1)
+                UUID uuid;
+                @ZoarialObjectElement(placement = 2)
+                String property;
+                @ZoarialObjectElement(placement = 3)
+                String description;
+            }
 
-        // Get info about a single action
-        @ZoarialNetworkObject
-        public static class InfoAction {
-            final static String[] order = {"action"};
-            UUID action;
-        }
+            // Get info about a single action
+            @Data
+            @ZoarialNetworkObject
+            public static class InfoAction {
+                @ZoarialObjectElement(placement = 1)
+                UUID action;
+            }
 
-        // Get a list of all actions
-        @ZoarialNetworkObject
-        public static class InfoActions {
-        }
+            // Get a list of all actions
+            @ZoarialNetworkObject
+            public static class InfoActions {
+            }
 
-        // Get general information about a node
-        @ZoarialNetworkObject
-        public static class InfoGeneral {
-        }
+            // Get general information about a node
+            @ZoarialNetworkObject
+            public static class InfoGeneral {
+            }
 
-        // Get a list of known nodes
-        @ZoarialNetworkObject
-        public static class InfoNodes {
+            // Get a list of known nodes
+            @ZoarialNetworkObject
+            public static class InfoNodes {
+            }
         }
     }
 }
